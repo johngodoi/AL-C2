23561d8fb88f8582752c8082ce2201d176751f70
John Godoi <john.godoi@gmail.com>
2013-01-22 23:04:35 -0200

    adaptando parser para usar automato

--- src/Parser.c
+++ src/Parser.c
@@ -13,19 +13,27 @@
 #include "Parser.h"
 #include "DefinicaoAutomato.h"
 #include "Automato.h"
+#include "TokenTypes.h"
 #include "stdlib.h"
+#include "stdio.h"
 
 //private declarations
-int analyse(Automato automato, int automatoState, char character);
-char * recoverWord(TArquivo *arquivo, int initialPositionWord);
+int callAutomatoAnalysis(Automato automato, int automatoState, char character);
+char * recoverWord(FILE *arquivo, int initialPositionWord, int lastPositionWord);
 
 void process(Automato automato, TArquivo *context, FILE *entrada, FILE *saida) {
-	int i;
-	for (i = 0; i < context->tam_file; i++) {
-		int currentAutomatoState = ERR;
+	int actualChar;
+	int lastCharAfterRecognizedLexem=0;
+	FILE *auxiliarPointer = entrada;
+	for (actualChar = 0; actualChar < context->tam_file; actualChar++) {
+		int currentAutomatoState = ERR;//FIXME for automato states
 		char character=fgetc(entrada);
-		currentAutomatoState = analyse(automato, currentAutomatoState, character);
+		currentAutomatoState = callAutomatoAnalysis(automato, currentAutomatoState, character);
 		if(currentAutomatoState == isFinalState(automato,currentAutomatoState)){
+			char *word = recoverWord(auxiliarPointer,lastCharAfterRecognizedLexem,actualChar);
+			auxiliarPointer=entrada;
+			currentAutomatoState=ERR;
+			lastCharAfterRecognizedLexem=actualChar+1;
 			//recoverWord from char after last char on last lexema recognized
 			//currentState is initial again
 			//write string readed and its result following exercise definition
@@ -34,28 +42,30 @@ void process(Automato automato, TArquivo *context, FILE *entrada, FILE *saida) {
 	rewind(entrada);
 }
 
-int analyse(Automato automato, int currentAutomatoState, char character) {
+int callAutomatoAnalysis(Automato automato, int currentAutomatoState, char character) {
 	//convert to a EntryType
 	//analyse for reserved word
 	//call automato analysis with currentState and character
 	return ERR;
 }
 
-char * recoverWord(TArquivo *arquivo, int initialPositionWord) {
-	int firstChar = arquivo->buffer_positions[initialPositionWord];
-	int lastChar;
-	if (initialPositionWord < arquivo->tam_positions - 1)
-		lastChar = arquivo->buffer_positions[initialPositionWord + 1] - 1;
-	else
-		lastChar = arquivo->tam_file;
+char * recoverWord(FILE *arquivo, int initialPositionWord, int lastPositionWord) {
+	int firstChar = initialPositionWord;//arquivo->buffer_positions[initialPositionWord];
+		int lastChar=lastPositionWord;
+	//if (initialPositionWord < arquivo->tam_positions - 1)
+	//lastChar = arquivo->buffer_positions[initialPositionWord + 1] - 1;
+	//else
+	//lastChar = arquivo->tam_file;
 	int i;
 	int j = 0;
+	char c;
 	int wordSize = lastChar - firstChar + 1;
 	char *word = (char*) malloc(sizeof(char) * wordSize);
 	word[wordSize - 1] = '\0';
 	for (i = firstChar; i < lastChar; i++) {
-		if (verifyEmptyChars(arquivo->buffer_original[i]) != 1)
-			word[j] = arquivo->buffer_original[i];
+		c=fgetc(arquivo);
+		if (verifyEmptyChars(c) != 1)
+			word[j] = c;
 		else
 			word[j] = '\0';
 		j++;
